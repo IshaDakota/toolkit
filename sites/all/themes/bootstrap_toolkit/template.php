f4231daccf0e94e435949f077f04be684625a56f
IshaDakota <adam@marchinglizard.net>
2014-12-28 14:36:04 -0600

    Add glyphicons to bullet fields.

--- sites/all/themes/bootstrap_toolkit/template.php
+++ sites/all/themes/bootstrap_toolkit/template.php
@@ -4,3 +4,25 @@
  * @file
  * template.php
  */
+
+function bootstrap_toolkit_preprocess_field(&$variables) {
+
+  // Add 'img-responsive' class to header images.
+  if($variables['element']['#field_name'] == 'field_header_image'){
+    foreach($variables['items'] as $key => $item){
+      $variables['items'][ $key ]['#item']['attributes']['class'][] = 'img-responsive'; 
+    }
+  }
+  
+  //Add glyphicons as "bullets" to core learning and key question fileds.
+  if($variables['element']['#field_name'] == 'field_core_learnings'){
+    foreach($variables['items'] as $key => $item){
+      $variables['items'][ $key ]['#prefix'] = '<span class="glyphicon glyphicon-ok-sign" aria-hidden="true"></span>';
+    }
+  }
+  if($variables['element']['#field_name'] == 'field_key_questions'){
+    foreach($variables['items'] as $key => $item){
+      $variables['items'][ $key ]['#prefix'] = '<span class="glyphicon glyphicon-question-sign" aria-hidden="true"></span>';
+    }
+  }
+}
