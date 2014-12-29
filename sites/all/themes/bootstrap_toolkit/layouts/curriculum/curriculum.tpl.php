213fbfe4663deeb18a2408bf91129655f2b6f187
IshaDakota <adam@marchinglizard.net>
2014-12-28 21:05:41 -0600

    Add custom layout for curricular content.

---
+++ sites/all/themes/bootstrap_toolkit/layouts/curriculum/curriculum.tpl.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @file
+ * Template for Panopoly Burr Flipped.
+ *
+ * Variables:
+ * - $css_id: An optional CSS id to use for the layout.
+ * - $content: An array of content, each item in the array is keyed to one
+ * panel of the layout. This layout supports the following sections:
+ */
+?>
+
+<div class="panel-display twocol-right clearfix <?php if (!empty($classes)) { print $classes; } ?><?php if (!empty($class)) { print $class; } ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+  <div class="row">
+    <div class="col-md-12 jumbotron panel-panel">
+      <div class="panel-panel-inner">
+        <?php print $content['jumbotron']; ?>
+      </div>
+    </div>
+  </div>  
+  <div class="row">
+    <div class="col-md-8 content panel-panel">
+      <div class="panel-panel-inner">
+        <?php print $content['contentmain']; ?>
+      </div>
+    </div>
+    <div class="col-md-4 sidebar panel-panel">
+      <div class="panel-panel-inner">
+        <?php print $content['sidebar']; ?>
+      </div>
+    </div>
+  </div>
+</div><!-- /.burr-flipped -->
