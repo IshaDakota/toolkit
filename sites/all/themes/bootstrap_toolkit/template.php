ece74e522fc7f874f573d7740bfe82ede6882a30
IshaDakota <adam@marchinglizard.net>
2014-12-28 21:06:26 -0600

    Add preprocess functions for button fields.

--- sites/all/themes/bootstrap_toolkit/template.php
+++ sites/all/themes/bootstrap_toolkit/template.php
@@ -13,7 +13,22 @@ function bootstrap_toolkit_preprocess_field(&$variables) {
       $variables['items'][ $key ]['#item']['attributes']['class'][] = 'img-responsive'; 
     }
   }
-  
+
+  //Make age range and program duration bootstrap buttons.
+  if($variables['element']['#field_name'] == 'field_age_range'){
+    foreach($variables['items'] as $key => $item){
+      $variables['items'][ $key ]['#options']['attributes']['class'] = 'btn btn-primary'; 
+      $variables['items'][ $key ]['#options']['attributes']['role'] = 'button';  
+    }
+  }
+
+  if($variables['element']['#field_name'] == 'field_program_duration'){
+    foreach($variables['items'] as $key => $item){
+      $variables['items'][ $key ]['#options']['attributes']['class'] = 'btn btn-primary'; 
+      $variables['items'][ $key ]['#options']['attributes']['role'] = 'button';  
+    }
+  }
+    
   //Add glyphicons as "bullets" to core learning and key question fileds.
   if($variables['element']['#field_name'] == 'field_core_learnings'){
     foreach($variables['items'] as $key => $item){
